e284931e279d7185f96d12bfee41f9a15d1365e0
K. Shankari <shankari@eecs.berkeley.edu>
2016-04-03 11:25:49 -0700

    Extend script to load data for a particular experiment run
    The name of the run is the last argument

--- bin/copy_timeline_files.sh
+++ bin/copy_timeline_files.sh
@@ -1,10 +1,13 @@
 DAY=$1
 TAG=$2
+RESULT_DIR=$3
 
-scp -i ~/.ssh/amplab-us-east.pem ubuntu@aws-webapp:/tmp/iphone.1.$TAG.$DAY results_dec_2015/ucb.sdb.ios.1/timeseries/$TAG.$DAY
-scp -i ~/.ssh/amplab-us-east.pem ubuntu@aws-webapp:/tmp/iphone.2.$TAG.$DAY results_dec_2015/ucb.sdb.ios.2/timeseries/$TAG.$DAY
-scp -i ~/.ssh/amplab-us-east.pem ubuntu@aws-webapp:/tmp/iphone.3.$TAG.$DAY results_dec_2015/ucb.sdb.ios.3/timeseries/$TAG.$DAY
+scp -i ~/.ssh/amplab-us-east.pem ubuntu@aws-webapp:/tmp/iphone.1.$TAG.$DAY $RESULT_DIR/ucb.sdb.ios.1/timeseries/$TAG.$DAY
+scp -i ~/.ssh/amplab-us-east.pem ubuntu@aws-webapp:/tmp/iphone.2.$TAG.$DAY $RESULT_DIR/ucb.sdb.ios.2/timeseries/$TAG.$DAY
+scp -i ~/.ssh/amplab-us-east.pem ubuntu@aws-webapp:/tmp/iphone.3.$TAG.$DAY $RESULT_DIR/ucb.sdb.ios.3/timeseries/$TAG.$DAY
+scp -i ~/.ssh/amplab-us-east.pem ubuntu@aws-webapp:/tmp/iphone.4.$TAG.$DAY $RESULT_DIR/ucb.sdb.ios.4/timeseries/$TAG.$DAY
 
-scp -i ~/.ssh/amplab-us-east.pem ubuntu@aws-webapp:/tmp/android.1.$TAG.$DAY results_dec_2015/ucb.sdb.android.1/timeseries/$TAG.$DAY
-scp -i ~/.ssh/amplab-us-east.pem ubuntu@aws-webapp:/tmp/android.2.$TAG.$DAY results_dec_2015/ucb.sdb.android.2/timeseries/$TAG.$DAY
-scp -i ~/.ssh/amplab-us-east.pem ubuntu@aws-webapp:/tmp/android.3.$TAG.$DAY results_dec_2015/ucb.sdb.android.3/timeseries/$TAG.$DAY
+scp -i ~/.ssh/amplab-us-east.pem ubuntu@aws-webapp:/tmp/android.1.$TAG.$DAY $RESULT_DIR/ucb.sdb.android.1/timeseries/$TAG.$DAY
+scp -i ~/.ssh/amplab-us-east.pem ubuntu@aws-webapp:/tmp/android.2.$TAG.$DAY $RESULT_DIR/ucb.sdb.android.2/timeseries/$TAG.$DAY
+scp -i ~/.ssh/amplab-us-east.pem ubuntu@aws-webapp:/tmp/android.3.$TAG.$DAY $RESULT_DIR/ucb.sdb.android.3/timeseries/$TAG.$DAY
+scp -i ~/.ssh/amplab-us-east.pem ubuntu@aws-webapp:/tmp/android.4.$TAG.$DAY $RESULT_DIR/ucb.sdb.android.4/timeseries/$TAG.$DAY
