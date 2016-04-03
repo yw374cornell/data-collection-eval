8fc050dd10d71502ae9980655c22bc41339b3c3d
K. Shankari <shankari@eecs.berkeley.edu>
2016-04-03 11:27:02 -0700

    Add a new script that creates all the result directories for a particular run
    This basically creates the directory structure that one would expect

---
+++ bin/create_result_dir.sh
@@ -0,0 +1,13 @@
+RESULT_DIR=$1
+
+echo "Creating $RESULT_DIR/ucb.sdb.android.{1,4}/timeseries"
+mkdir -p $RESULT_DIR/ucb.sdb.android.1/timeseries
+mkdir -p $RESULT_DIR/ucb.sdb.android.2/timeseries
+mkdir -p $RESULT_DIR/ucb.sdb.android.3/timeseries
+mkdir -p $RESULT_DIR/ucb.sdb.android.4/timeseries
+
+echo "Creating $RESULT_DIR/ucb.sdb.ios.{1,4}/timeseries"
+mkdir -p $RESULT_DIR/ucb.sdb.ios.1/timeseries
+mkdir -p $RESULT_DIR/ucb.sdb.ios.2/timeseries
+mkdir -p $RESULT_DIR/ucb.sdb.ios.3/timeseries
+mkdir -p $RESULT_DIR/ucb.sdb.ios.4/timeseries
