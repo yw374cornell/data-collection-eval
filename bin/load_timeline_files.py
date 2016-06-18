69604e01e0ca6ed867af7721651fcc891666ca89
K. Shankari <shankari@eecs.berkeley.edu>
2016-06-17 21:54:30 -0700

    Deal with fallout of rename all the files as .timeline
    - .gitattribute becomes much simpler
    - scripts have to be modified to use the correct filenames

--- bin/load_timeline_files.py
+++ bin/load_timeline_files.py
@@ -5,20 +5,20 @@ RESULT_DIR=$3
 # Change this to point to the location of your server code
 EMISSION_SERVER_HOME=$HOME/e-mission/e-mission-server
 
-echo "Loading file $RESULT_DIR/ucb.sdb.ios.1/timeseries/$TAG.$DAY"
-PYTHONPATH=$EMISSION_SERVER_HOME:$PYTHONPATH python $EMISSION_SERVER_HOME/bin/debug/load_timeline_for_day_and_user.py $PWD/$RESULT_DIR/ucb.sdb.ios.1/timeseries/$TAG.$DAY
-echo "Loading file $RESULT_DIR/ucb.sdb.ios.2/timeseries/$TAG.$DAY"
-PYTHONPATH=$EMISSION_SERVER_HOME:$PYTHONPATH python $EMISSION_SERVER_HOME/bin/debug/load_timeline_for_day_and_user.py $PWD/$RESULT_DIR/ucb.sdb.ios.2/timeseries/$TAG.$DAY
-echo "Loading file $RESULT_DIR/ucb.sdb.ios.3/timeseries/$TAG.$DAY"
-PYTHONPATH=$EMISSION_SERVER_HOME:$PYTHONPATH python $EMISSION_SERVER_HOME/bin/debug/load_timeline_for_day_and_user.py $PWD/$RESULT_DIR/ucb.sdb.ios.3/timeseries/$TAG.$DAY
-echo "Loading file $RESULT_DIR/ucb.sdb.ios.4/timeseries/$TAG.$DAY"
-PYTHONPATH=$EMISSION_SERVER_HOME:$PYTHONPATH python $EMISSION_SERVER_HOME/bin/debug/load_timeline_for_day_and_user.py $PWD/$RESULT_DIR/ucb.sdb.ios.4/timeseries/$TAG.$DAY
+echo "Loading file $RESULT_DIR/ucb.sdb.ios.1/timeseries/$TAG.$DAY.timeline"
+PYTHONPATH=$EMISSION_SERVER_HOME:$PYTHONPATH python $EMISSION_SERVER_HOME/bin/debug/load_timeline_for_day_and_user.py $PWD/$RESULT_DIR/ucb.sdb.ios.1/timeseries/$TAG.$DAY.timeline
+echo "Loading file $RESULT_DIR/ucb.sdb.ios.2/timeseries/$TAG.$DAY.timeline"
+PYTHONPATH=$EMISSION_SERVER_HOME:$PYTHONPATH python $EMISSION_SERVER_HOME/bin/debug/load_timeline_for_day_and_user.py $PWD/$RESULT_DIR/ucb.sdb.ios.2/timeseries/$TAG.$DAY.timeline
+echo "Loading file $RESULT_DIR/ucb.sdb.ios.3/timeseries/$TAG.$DAY.timeline"
+PYTHONPATH=$EMISSION_SERVER_HOME:$PYTHONPATH python $EMISSION_SERVER_HOME/bin/debug/load_timeline_for_day_and_user.py $PWD/$RESULT_DIR/ucb.sdb.ios.3/timeseries/$TAG.$DAY.timeline
+echo "Loading file $RESULT_DIR/ucb.sdb.ios.4/timeseries/$TAG.$DAY.timeline"
+PYTHONPATH=$EMISSION_SERVER_HOME:$PYTHONPATH python $EMISSION_SERVER_HOME/bin/debug/load_timeline_for_day_and_user.py $PWD/$RESULT_DIR/ucb.sdb.ios.4/timeseries/$TAG.$DAY.timeline
 
-echo "Loading file $RESULT_DIR/ucb.sdb.android.1/timeseries/$TAG.$DAY"
-PYTHONPATH=$EMISSION_SERVER_HOME:$PYTHONPATH python $EMISSION_SERVER_HOME/bin/debug/load_timeline_for_day_and_user.py $PWD/$RESULT_DIR/ucb.sdb.android.1/timeseries/$TAG.$DAY
-echo "Loading file $RESULT_DIR/ucb.sdb.android.2/timeseries/$TAG.$DAY"
-PYTHONPATH=$EMISSION_SERVER_HOME:$PYTHONPATH python $EMISSION_SERVER_HOME/bin/debug/load_timeline_for_day_and_user.py $PWD/$RESULT_DIR/ucb.sdb.android.2/timeseries/$TAG.$DAY
-echo "Loading file $RESULT_DIR/ucb.sdb.android.3/timeseries/$TAG.$DAY"
-PYTHONPATH=$EMISSION_SERVER_HOME:$PYTHONPATH python $EMISSION_SERVER_HOME/bin/debug/load_timeline_for_day_and_user.py $PWD/$RESULT_DIR/ucb.sdb.android.3/timeseries/$TAG.$DAY
-echo "Loading file $RESULT_DIR/ucb.sdb.android.4/timeseries/$TAG.$DAY"
-PYTHONPATH=$EMISSION_SERVER_HOME:$PYTHONPATH python $EMISSION_SERVER_HOME/bin/debug/load_timeline_for_day_and_user.py $PWD/$RESULT_DIR/ucb.sdb.android.4/timeseries/$TAG.$DAY
+echo "Loading file $RESULT_DIR/ucb.sdb.android.1/timeseries/$TAG.$DAY.timeline"
+PYTHONPATH=$EMISSION_SERVER_HOME:$PYTHONPATH python $EMISSION_SERVER_HOME/bin/debug/load_timeline_for_day_and_user.py $PWD/$RESULT_DIR/ucb.sdb.android.1/timeseries/$TAG.$DAY.timeline
+echo "Loading file $RESULT_DIR/ucb.sdb.android.2/timeseries/$TAG.$DAY.timeline"
+PYTHONPATH=$EMISSION_SERVER_HOME:$PYTHONPATH python $EMISSION_SERVER_HOME/bin/debug/load_timeline_for_day_and_user.py $PWD/$RESULT_DIR/ucb.sdb.android.2/timeseries/$TAG.$DAY.timeline
+echo "Loading file $RESULT_DIR/ucb.sdb.android.3/timeseries/$TAG.$DAY.timeline"
+PYTHONPATH=$EMISSION_SERVER_HOME:$PYTHONPATH python $EMISSION_SERVER_HOME/bin/debug/load_timeline_for_day_and_user.py $PWD/$RESULT_DIR/ucb.sdb.android.3/timeseries/$TAG.$DAY.timeline
+echo "Loading file $RESULT_DIR/ucb.sdb.android.4/timeseries/$TAG.$DAY.timeline"
+PYTHONPATH=$EMISSION_SERVER_HOME:$PYTHONPATH python $EMISSION_SERVER_HOME/bin/debug/load_timeline_for_day_and_user.py $PWD/$RESULT_DIR/ucb.sdb.android.4/timeseries/$TAG.$DAY.timeline
